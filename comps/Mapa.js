e35afcfce38336a07218dc3a9da1be4da915403b
ydrea <ydrea.wrld@gmail.com>
2022-09-23 19:22:23 +0200

    ..

--- comps/Mapa.js
+++ comps/Mapa.js
@@ -2,6 +2,8 @@ import * as React from "react";
 import MapView, { Callout, Marker } from "react-native-maps";
 import { StyleSheet, Text, View, Dimensions } from "react-native";
 //
+
+//
 export default function Map() {
   return (
     <View style={styles.container}>
