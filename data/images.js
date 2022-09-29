1ba2287ac6be599a5355008fc66253768c5f9591
ydrea <ydrea.wrld@gmail.com>
2022-09-29 14:27:15 +0200

    images synced

--- data/images.js
+++ data/images.js
@@ -2,12 +2,12 @@ export const images = {
   sculptures: {
     0: require("../assets/img/Kumica.jpg"),
     1: require("../assets/img/Senoa.jpg"),
-    2: require("../assets/img/Matos.jpg"),
-    3: require("../assets/img/Ujevic.jpg"),
-    4: require("../assets/img/Martic.jpg"),
+    2: require("../assets/img/Martic.jpg"),
+    3: require("../assets/img/Strossmayer.jpg"),
+    4: require("../assets/img/Ujevic.jpg"),
     5: require("../assets/img/Krleza.jpg"),
-    6: require("../assets/img/Zagorka.jpg"),
-    7: require("../assets/img/Tesla.jpg"),
+    6: require("../assets/img/Matos.jpg"),
+    7: require("../assets/img/Zagorka.jpg"),
     8: require("../assets/img/Congratulation.jpg"),
   },
 };
