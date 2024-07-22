0d08c20d1271d31d953dbcaef9b7b1a847d65c58
ydrea <ydrea.wrld@gmail.com>
2024-07-22 22:13:49 +0200

    cleaned

--- android/app/src/main/jni/MainComponentsRegistry.h
+++ android/app/src/main/jni/MainComponentsRegistry.h
@@ -13,7 +13,7 @@ class MainComponentsRegistry
  public:
   // Adapt it to the package you used for your Java class.
   constexpr static auto kJavaDescriptor =
-      "Lcom/ydrea/thegamezagrebeta/newarchitecture/components/MainComponentsRegistry;";
+      "Lcom/andrijavr/thegamezagreb/newarchitecture/components/MainComponentsRegistry;";
 
   static void registerNatives();
 
